e864f54392dc9d5007f34df6b0aba47b2124131b
AmitKumarDe <amitasn2021@gmail.com>
2026-06-13 19:10:31 +0530

    feat(backend): configure express application and server startup
    - Add cors and cookie-parser dependencies
    - Configure express middleware including cors, json, and cookie-parser in app.js
    - Update index.js to start the server after successful database connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 // Import dotenv
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 console.log("URI:", process.env.MONGODB_URI);
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port :${process.env.PORT}`);
+    });
+  })
+  .catch(() => {
+    console.log("MONGO db connection failed !! ", error);
+  });
